c908f7ad4bd31722f0b935042ce34025c9b5c6f8
Greg Beaty <greg.beaty2@gmail.com>
2012-09-21 17:43:58 -0400

    Base commit

---
+++ src/Terminal.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : Terminal.c
+ Author      : Greg Beaty
+ Version     :
+ Copyright   : 2012
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Hello Terminal!"); /* prints Hello Terminal! */
+	return EXIT_SUCCESS;
+}
