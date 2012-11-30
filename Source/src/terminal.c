ae8dcf995ef154435b1d5f16e367a143c47690fb
Greg Beaty <greg.beaty2@gmail.com>
2012-11-30 16:21:48 -0500

    Reverts the streaming script setup.

--- Source/src/terminal.c
+++ Source/src/terminal.c
@@ -39,12 +39,11 @@ void error(char *msg) {
 	exit(1);
 }
 
-int control_stream(Socket *sock, int start) {
+char *control_stream(Socket *sock, int start) {
 	if (start) {
-		return media_stream_init(comm_get_cli_host_addr(sock), STREAM_PORT);
-	}else {
-		return media_stream_end();
+		media_stream_init(comm_get_cli_host_addr(sock), STREAM_PORT);
 	}
+	return ""; //TODO: Return success/failure
 }
 
 void close_relay() {
@@ -79,7 +78,11 @@ parse_input(Socket *sock, char *input) {
 			return json_object_to_json_string(filesys_get_profiles());
 		} else if (strcmp(str, "SendProfiles") == 0) {
 			return json_object_to_json_string(filesys_save_profiles(jobj));
-		}  else if (strcmp(str, "CloseRelay") == 0) {
+		} else if (strcmp(str, "StartStream") == 0) {
+			return control_stream(sock, 1);
+		} else if (strcmp(str, "StopStream") == 0) {
+			return control_stream(sock, 0);
+		} else if (strcmp(str, "CloseRelay") == 0) {
 			close_relay();
 			//TODO: Does this need a response?
 			return "";
@@ -142,8 +145,6 @@ void *start_tcp_server(void *arg) {
 
 			printf("INFO: Client connected - %s\n",
 					comm_get_cli_host_info(client));
-
-			control_stream(client, 1);
 			while (1) {
 				memset(&buf, 0, sizeof(buf));
 				bytes = comm_receive(client, buf, sizeof(buf));
@@ -161,7 +162,6 @@ void *start_tcp_server(void *arg) {
 				}
 			}
 
-			control_stream(client, 0);
 			printf("INFO: Client disconnected\n");
 		}
 	}
